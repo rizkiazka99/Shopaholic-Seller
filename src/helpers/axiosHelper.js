ad083091f945a075213f81ef059f9d0913dfdd39
rizkiazkafihiaghnia996 <rizkiazkafihiaghnia996@gmail.com>
2023-06-09 16:31:06 +0700

    ...

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -1,3 +1,4 @@
-const baseUrl = 'http://localhost:3000/api';
+//const baseUrl = 'http://localhost:3000/api';
+const baseUrl = 'https://bumpy-flat-animantarx.glitch.me/api';
 
 export { baseUrl };
--- src/helpers/commonVariables.js
+++ src/helpers/commonVariables.js
@@ -1,4 +1,5 @@
-const baseApiUrl = "http://localhost:3000/";
+//const baseApiUrl = "http://localhost:3000/";
+const baseApiUrl = "https://bumpy-flat-animantarx.glitch.me/";
 const swalConfirmButtonColor = '#ADFF2F';
 const swalCancelButtonColor = '#d33';
 
